99ca309297eeb9b349b17cb760e7e753f35e267d
ftao <devnull@localhost>
2011-09-05 22:05:45 +0800

    Basic Struction

---
+++ README.rst
@@ -0,0 +1,14 @@
+Django Freeradius Admin 
+=========================
+
+Basic Features
+----------------
+
+API and WEB UI for 
+
+* add user
+* change user password 
+* suspend user
+* add/edit/delete group
+* add/remove user to/from group
+
--- djra/settings.py
+++ djra/settings.py
@@ -4,17 +4,17 @@ DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
 ADMINS = (
-    # ('Your Name', 'your_email@example.com'),
+    ('ftao', 'ftao@ftao.org'),
 )
 
 MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '',                      # Or path to database file if using sqlite3.
-        'USER': '',                      # Not used with sqlite3.
-        'PASSWORD': '',                  # Not used with sqlite3.
+        'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'radius',                      # Or path to database file if using sqlite3.
+        'USER': 'radius',                      # Not used with sqlite3.
+        'PASSWORD': 'radius-dev-pass',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
     }
@@ -116,9 +116,10 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # Uncomment the next line to enable the admin:
-    # 'django.contrib.admin',
+    'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
+    'freeradius',
 )
 
 # A sample logging configuration. The only tangible logging
