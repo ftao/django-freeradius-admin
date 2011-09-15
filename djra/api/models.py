a76826820547fd4feb77e8346199ac145cfb10d1
ftao <devnull@localhost>
2011-09-15 22:27:50 +0800

    show somethings at dashboard

--- djra/api/models.py
+++ djra/api/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
-# Create your models here.
+from django.db.models import Q
+from freeradius.models import Radusergroup,Radcheck,Radgroupcheck,Radacct
+
+def get_raduser_count():
+    total_count = Radcheck.objects.filter(attribute='User-Password', op=':=')\
+                                  .values('username')\
+                                  .distinct().count()
+    suspended_count = Radcheck.objects.filter(attribute='Auth-Type',
+                                              op=':=',
+                                              value='Reject')\
+                                      .values('username')\
+                                      .distinct().count()
+ 
+    return {
+        'total' : total_count,
+        'suspended' : suspended_count,
+        'active' : total_count - suspended_count
+    }
+
+
+def get_radgroup_count():
+    groups = list(Radusergroup.objects.values_list('groupname', flat=True).distinct())
+    groups += list(Radgroupcheck.objects.values_list('groupname', flat=True).distinct())
+
+    return len(set(groups))
+
--- djra/radmin/views.py
+++ djra/radmin/views.py
@@ -3,11 +3,17 @@ from django.shortcuts import render_to_response
 from django.core.urlresolvers import reverse
 from django.http import HttpResponseRedirect, HttpResponseForbidden,HttpResponse,HttpResponseBadRequest
 from django.conf import settings
+from djra.api.models import get_raduser_count,get_radgroup_count
 
 def home(request):
+    user_count = get_raduser_count()
+    group_count = get_radgroup_count()
     return render_to_response(
         'radmin/home.html',
-        {},
+        {'active_user_count' : user_count['active'],
+         'suspended_user_count' : user_count['suspended'],
+         'group_count' : group_count,
+        },
         context_instance = RequestContext(request)
     )
 
