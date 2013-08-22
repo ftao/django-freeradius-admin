7454afc9158fc146e0b36a670011d18680e0ee8a
ftao <filia.tao@gmail.com>
2013-08-22 21:30:29 +0800

    fix weired problem under gunicorn

--- djra/freeradius/views.py
+++ djra/freeradius/views.py
@@ -1,2 +1,7 @@
 # -*- coding: utf-8 -*-
 
+from django.db import models
+from django.contrib.auth.models import User
+from tastypie.models import create_api_key
+models.signals.post_save.connect(create_api_key, sender=User)
+
